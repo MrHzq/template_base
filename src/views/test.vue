c10c62f7541ad4a8e45bd7fb6280852c2d9b785d
894568474 <894568474@qq.com>
2018-11-08 11:33:55 +0800

    初始化

---
+++ src/views/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="test">
+        我是base
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {};
+        },
+        created() {
+            this.$api.logout({
+                password: 'qwer1234',
+                userNo: '13402318457'
+            });
+        },
+        methods: {}
+    };
+</script>
+
+<style scoped>
+    .test {
+        color: red;
+    }
+</style>
