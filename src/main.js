dc66719563109f84823df02569029e635dfeaedc
894568474 <894568474@qq.com>
2018-12-13 10:45:22 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -10,17 +10,41 @@ Vue.use(hzqTool)
 Vue.use(hzqAxios, apiUrl, {
     baseURL: 'https://open-api.beone.app',
     preURL: 'https://api-t.xkt.one',
-    prodURL: 'https://api.dzqqsc.com'
+    prodURL: 'https://api.dzqqsc.com',
+    createConfig: {
+        headers: {
+            'X-APPID': 'x9UdyFXeEwMp'
+        }
+    },
+    beforeRequest(config) {
+        let token = sessionStorage.getItem('xkt_qazplm')
+        if (token) {
+            config.headers['X-TOKEN'] = JSON.parse(token)
+        }
+        if (config.url === '/web/analyst/logout') {
+            sessionStorage.clear()
+        }
+        return config
+    },
+    respSuccess(resp) {
+        if (resp.data.code !== 0) {
+            console.error(resp.data.msg)
+            if (resp.data.code === 99999) {
+                sessionStorage.clear()
+                router.push('/login')
+            }
+        }
+    },
+    respError(error) {
+        if (
+            error.config.url.match('getVideoVsampleInfo') ||
+            error.config.url.match('getVideoTransCodeInfo')
+        ) {
+        } else {
+            console.error('网络异常，请稍后重试')
+        }
+    }
 })
-
-const prefix = 'xkt_'
-Vue.prototype.$getItem = key => {
-    return JSON.parse(window.sessionStorage.getItem(prefix + key))
-}
-Vue.prototype.$setItem = (key, data) => {
-    window.sessionStorage.setItem(prefix + key, JSON.stringify(data))
-}
-Vue.prototype.$copy = obj => JSON.parse(JSON.stringify(obj))
 
 Vue.config.productionTip = false
 
